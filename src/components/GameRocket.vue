c22cdf6feebdde2a25712303f17be4a01399a33e
Ryan Grosso <ryan.grosso@btinternet.com>
2021-10-10 20:31:51 +0100

    Initial implementation of the Rocket animation

---
+++ src/components/GameRocket.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="rocket">
+        <div class="rocket-body">
+            <div class="body" />
+            <div class="fin fin-left" />
+            <div class="fin fin-right" />
+            <div class="window" />
+        </div>
+        <div class="exhaust-flame" />
+    </div>
+</template>
--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -1,14 +1,22 @@
 <template>
-    <div id="game" class="col-12 col-md-8 p-2">
-        <div id="game-square" class="d-flex justify-content-center align-items-center">
-            <h2 v-if="gameRoundActive" class="mb-0 monospace">{{ multipler }}</h2>
-            <h3 v-else>Preparing Round...</h3>
+    <div id="game" class="col-12 col-md-8 p-2" :class="`game-active-${gameRoundActive}`">
+        <div
+            id="game-square"
+            class="d-flex justify-content-center align-items-center position-relative overflow-hidden"
+        >
+            <GameRocket />
+            <div class="position-absolute" style="top: 5%">
+                <h2 v-if="gameRoundActive" class="mb-0 monospace">{{ multipler }}</h2>
+                <h3 v-else>Preparing Round...</h3>
+            </div>
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
 import { inject, watchEffect, ref } from "vue";
 import { Store } from "../interfaces/store";
+import GameRocket from "./GameRocket.vue";
 
 const store = <Store>inject("store");
 const multipler = ref(store.getters.multiplier());
